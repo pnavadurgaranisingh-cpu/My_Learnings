d00378d53c7107cd5a27ccfd152eebe068541653
Nava durga rani singh .p <pnavadurgaranisingh@gmail.com>
2026-04-24 20:04:38 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+#string concatination
+a = "hi!"
+b = "Everyone"
+print(a+" "+b)
+
+#string repetation
+greetings = "good_morning"
+print((greetings)*2)
+
+#string membership
+string = "Python programming is a high level language"
+print("are" in string )
+print("the" not in string)
