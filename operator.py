b8a2689c188ffddb3c6410520846471d4981bcbb
Nava durga rani singh .p <pnavadurgaranisingh@gmail.com>
2026-04-25 20:22:28 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,7 @@
+#initilization of variable
+a = 5
+b = 10
+print("sum of a and b: ",a + b)
+print("difference b/w a and b: ",a - b)
+print("multiplication of a and b: ",a*b)
+print("ratio b/w a and b: ",a/b)
